47c06c498fcd92ad98a8d64554f36cc65a22ab77
Christian Rohmann <christian.rohmann@inovex.de>
2019-01-09 11:21:29 +0100

    Update Ruby to 2.5, yarn to current (remove pinning to 1.0.2-1), Golang to 1.11.4 and install ruby bundler in version 1.17.3

--- assets/build/20_install.sh
+++ assets/build/20_install.sh
@@ -22,7 +22,8 @@ update-locale LANG=C.UTF-8 LC_MESSAGES=POSIX
 locale-gen en_US.UTF-8
 dpkg-reconfigure locales
 
-gem install --no-document bundler
+gem install --no-document bundler -v 1.17.3 \
+
 
 # https://en.wikibooks.org/wiki/Grsecurity/Application-specific_Settings#Node.js
 paxctl -Cm `which nodejs`
