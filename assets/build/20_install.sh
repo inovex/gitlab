bc63a4acda4cdb6432835ef0fdc50c5a055f4670
Sascha Askani <sascha.askani@inovex.de>
2019-02-05 11:09:34 +0100

    add missing case

--- assets/build/20_install.sh
+++ assets/build/20_install.sh
@@ -137,6 +137,9 @@ echo "Setup gitaly..."
 
 cd ${GITLAB_INSTALL_DIR?}
 case ${GITLAB_VERSION?} in
+11.7.*)
+  exec_as_git bundle exec rake "gitlab:gitaly:install[${GITALY_INSTALL_DIR?},${GITLAB_REPOS_DIR?}]"
+;;
 11.6.*)
   exec_as_git bundle exec rake "gitlab:gitaly:install[${GITALY_INSTALL_DIR?},${GITLAB_REPOS_DIR?}]"
 ;;
